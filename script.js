753562d8dca3b3da9e48ec2e59f6be93cc0a6957
Mauricio <mauabe@gmx.com>
2019-05-01 16:00:15 -0700

    completed CSS and added JS for modal

---
+++ script.js
@@ -0,0 +1,14 @@
+// console.log('hey there');
+
+const showModal = function() {
+  let popup = document.getElementById('modal');
+  popup.classList.add('show');
+};
+
+
+const hideModal = function(){
+  let popup = document.getElementById('modal');
+  popup.classList.remove('show');
+
+};
+   
